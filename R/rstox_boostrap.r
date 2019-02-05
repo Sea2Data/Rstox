9786c6032ba906bc9b43d2b31d8063b931b19a00
Holmin <arnejh@HI-10976.hi.no>
2019-02-05 12:45:45 +0100

    Added browser()

--- R/rstox_boostrap.r
+++ R/rstox_boostrap.r
@@ -130,6 +130,8 @@ bootstrapParallel <- function(projectName, assignments, psuNASC=NULL, stratumNAS
 	### 	stop("Either projectName or baseline must be given.")
 	### }
  
+ 	browser()
+ 	
 	# Allow for inputs given in 'nboot' and 'seed' to prepare for the higher level functions bootstrapAcoustic() and runBootstrap():
 	if(length(parameters$nboot)){
 		nboot <- parameters$nboot
