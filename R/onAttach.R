04eda49e219a21658e8e74bc362fd676605cdf7f
Arne Johannes Holmin <arnejh@imr.no>
2019-05-05 22:50:29 +0200

    Added the RstoxBuild package

--- R/onAttach.R
+++ R/onAttach.R
@@ -1,9 +1,3 @@
 .onAttach <- function(libname, pkgname){
-	
-	packageStartupMessage("Rstox_1.11.1
-**********
-WARNING: This version of Rstox is an unofficial/developer version and bugs should be expected.
-If problems with Java Memory such as java.lang.OutOfMemoryError occurs, see ?setJavaMemory.
-**********
-", appendLF=FALSE)
+	packageStartupMessage("Rstox_1.11.1 \n**********\nWARNING: This version of Rstox is an unofficial/developer version and bugs should be expected.\nIf problems with Java Memory such as java.lang.OutOfMemoryError occurs, see ?setJavaMemory.\n**********\n", appendLF=FALSE)
 }
--- R/onLoad.R
+++ R/onLoad.R
@@ -1,9 +1,7 @@
 .onLoad <- function(libname, pkgname){
-	
 	if(Sys.getenv("JAVA_HOME")!="") Sys.setenv(JAVA_HOME="")
-	# options(java.parameters="-Xmx2g")
 	# Initiate the Rstox envitonment:
 	Definitions <- initiateRstoxEnv()
 	# Set the Java memory:
 	setJavaMemory(Definitions$JavaMem)
-}
+} 
