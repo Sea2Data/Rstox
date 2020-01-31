513da20f8820efb1b9bfd92537880faf0e9cb20b
edvinf <edvin.fuglebakk@gmail.com>
2020-01-31 20:52:20 +0100

    Reintroduce build-time print

--- R/onAttach.R
+++ R/onAttach.R
@@ -1,3 +1,5 @@
+x <- Sys.time()
+
 .onAttach <- function(libname, pkgname){
-	packageStartupMessage("Rstox_1.11.1 \n**********\nWARNING: This version of Rstox is an unofficial/developer version and bugs should be expected.\nIf problems with Java Memory such as java.lang.OutOfMemoryError occurs, see ?setJavaMemory.\n**********\n", appendLF=FALSE)
+  packageStartupMessage("Rstox_1.11.1 \n**********\nWARNING: This version of Rstox is an unofficial/developer version and bugs should be expected.\nBuild time:", x,"\nIf problems with Java Memory such as java.lang.OutOfMemoryError occurs, see ?setJavaMemory.\n**********\n", appendLF=FALSE)
 }
